93401a2e1a4256bc82f2c055cde26843efc05404
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-12-28 23:41:04 +0300

    v3.1

--- src/components/WhyUs/index.tsx
+++ src/components/WhyUs/index.tsx
@@ -4,6 +4,7 @@ import SectionTitle from "../Common/SectionTitle";
 import Image from "next/image";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { CheckCircleIcon, CheckIcon } from "@heroicons/react/solid"; // Icône pour les listes
 export default function WhyUs() {
   useEffect(() => {
     AOS.init({
@@ -13,16 +14,32 @@ export default function WhyUs() {
   }, []);
   return (
     <div className="relative  py-10 dark:bg-gray-dark">
-      <div className="container">
-        <div className="text-center">
-          <SectionTitle
-            title=" Pourquoi Choisir E-Media ?"
-            paragraph=" Découvrez les raisons qui font d'E-Media un choix idéal pour
-      votre avenir dans les industries créatives."
-            center
-          />
+      <div
+        className="relative bg-cover bg-fixed bg-center px-6 py-16 text-white"
+        style={{
+          backgroundImage: "url('/images/banniere/p1.png')", // Remplacez par l'URL de votre image
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-40"></div>{" "}
+        {/* Overlay pour améliorer la lisibilité */}
+        <div className="relative z-10 mx-auto max-w-7xl text-center">
+          {/* Titre principal */}
+          <h2
+            className="mb-4 text-4xl font-extrabold text-white md:text-5xl"
+            data-aos="fade-up"
+          >
+            Pourquoi Choisir E-Media ?
+          </h2>
+
+          {/* Sous-titre */}
+          <p className="mx-auto max-w-3xl text-lg font-light leading-relaxed md:text-xl">
+            Découvrez les raisons qui font d'E-Media un choix idéal pour votre
+            avenir dans les industries créatives.
+          </p>
         </div>
-        <div className="bg-gray-100 px-6 py-12 dark:bg-gray-dark">
+      </div>
+      <div className="container">
+        <div className=" px-6 py-12 dark:bg-gray-dark">
           <div
             className="mx-auto max-w-4xl overflow-hidden rounded-lg bg-white shadow-lg"
             data-aos="fade-up"
@@ -55,7 +72,7 @@ export default function WhyUs() {
             </div>
           </div>
         </div>
-        <div className=" px-6 py-12 dark:bg-gray-dark">
+        <div className="space-y-16 px-6 py-12 dark:bg-gray-dark">
           {/*  <!-- Section 1: Une Formation en Ligne Accessible Partout -->*/}
           <div className="mx-auto max-w-7xl space-y-16">
             <div
@@ -63,33 +80,45 @@ export default function WhyUs() {
               data-aos="fade-up"
             >
               <div className="p-6 md:w-1/2" data-aos="fade-right">
-                <h2 className="mb-4 text-2xl font-bold text-black dark:text-white md:text-3xl">
+                <h2 className="mb-4 text-2xl font-bold text-primary dark:text-white md:text-3xl">
                   Une Formation en Ligne Accessible Partout
                 </h2>
-                <p className="mb-6 text-lg leading-relaxed text-gray-700">
+                <p className="mb-6 text-lg leading-relaxed text-gray-500 dark:text-gray-400">
                   E-Media propose des programmes en ligne de haute qualité,
                   accessibles à Madagascar et à l&apos;international.
                 </p>
-                <ul className="list-inside list-disc space-y-3 text-gray-700">
-                  <li>
-                    <span className="font-semibold">
-                      Une communauté étudiante diversifiée
-                    </span>
-                    , issue d&apos;Afrique, d&apos;Europe, des Amériques et de
-                    la diaspora malgache.
+                <ul className="space-y-3 text-gray-500 dark:text-gray-400">
+                  <li className=" flex items-center">
+                    <div className="mr-4">
+                      <CheckIcon className=" h-6 w-6 text-green-500" />
+                    </div>
+
+                    <div>
+                      <span className="font-semibold">
+                        Des formations flexibles
+                      </span>{" "}
+                      et un accompagnement personnalisé adaptés aux besoins des
+                      apprenants.
+                    </div>
                   </li>
-                  <li>
-                    <span className="font-semibold">
-                      Des formations flexibles
-                    </span>{" "}
-                    et un accompagnement personnalisé adaptés aux besoins des
-                    apprenants.
+                  <li className=" flex items-center">
+                    <div className="mr-4">
+                      <CheckIcon className=" h-6 w-6 text-green-500" />
+                    </div>
+
+                    <div>
+                      <span className="font-semibold">
+                        Une communauté étudiante diversifiée
+                      </span>
+                      , issue d&apos;Afrique, d&apos;Europe, des Amériques et de
+                      la diaspora malgache.
+                    </div>
                   </li>
                 </ul>
               </div>
               <div className="md:w-1/2" data-aos="fade-left">
                 <Image
-                  src="/images/banniere/p4.png"
+                  src="/images/why/online.jpg"
                   alt="Formation en ligne"
                   className="h-64 w-full rounded-lg object-cover shadow-lg md:h-80"
                   width={500}
@@ -117,29 +146,54 @@ export default function WhyUs() {
               className="order-1 p-6 md:order-2 md:w-1/2"
               data-aos="fade-left"
             >
-              <h2 className="mb-4 text-2xl font-bold  text-black dark:text-white md:text-3xl">
+              <h2 className="mb-4 text-2xl font-bold  text-primary dark:text-white md:text-3xl">
                 Des Infrastructures Modernes
               </h2>
-              <p className="mb-6 text-lg leading-relaxed text-gray-700">
+              <p className="mb-6 text-lg leading-relaxed text-gray-500 dark:text-gray-400">
                 Avec des équipements de pointe, E-Media garantit une formation
                 pratique et immersive :
               </p>
-              <ul className="list-inside list-disc space-y-3 text-gray-700">
-                <li>
-                  Studios professionnels : Fond vert, plateau TV, sonorisation,
-                  et régie vidéo.
+              <ul className="list-inside list-disc space-y-3 text-gray-500 dark:text-gray-400">
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Studios professionnels : Fond vert, plateau TV,
+                    sonorisation, et régie vidéo.
+                  </div>
                 </li>
-                <li>
-                  Parc informatique : Ordinateurs performants (Core i7, 7ᵉ
-                  génération et plus).
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Parc informatique : Ordinateurs performants (Core i7, 7ᵉ
+                    génération et plus).
+                  </div>
                 </li>
-                <li>
-                  Laboratoires spécialisés : Multimédia, informatique et
-                  techniques avancées.
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Laboratoires spécialisés : Multimédia, informatique et
+                    techniques avancées.
+                  </div>
                 </li>
-                <li>
-                  Cabines de son et studio radiophonique pour une qualité
-                  optimale.
+
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Cabines de son et studio radiophonique pour une qualité
+                    optimale.
+                  </div>
                 </li>
               </ul>
             </div>
@@ -152,33 +206,51 @@ export default function WhyUs() {
           >
             <div className="p-6 md:w-1/2">
               <h2
-                className="mb-4 text-2xl font-bold text-black dark:text-white md:text-3xl"
+                className="mb-4 text-2xl font-bold text-primary dark:text-white md:text-3xl"
                 data-aos="fade-right"
               >
                 Un Engagement pour le Développement Durable
               </h2>
-              <p className="mb-6 text-lg leading-relaxed text-gray-700">
+              <p className="mb-6 text-lg leading-relaxed text-gray-500 dark:text-gray-400">
                 E-Media contribue au développement socio-économique de
                 Madagascar et de l&apos;Afrique en :
               </p>
-              <ul className="list-inside list-disc space-y-3 text-gray-700">
-                <li>
-                  Créant des opportunités professionnelles pour les jeunes dans
-                  les industries créatives.
+              <ul className="list-inside list-disc space-y-3 text-gray-500 dark:text-gray-400">
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Créant des opportunités professionnelles pour les jeunes
+                    dans les industries créatives.
+                  </div>
                 </li>
-                <li>
-                  Encourageant l&apos;entrepreneuriat et l&apos;innovation comme
-                  moteurs de croissance.
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Encourageant l&apos;entrepreneuriat et l&apos;innovation
+                    comme moteurs de croissance.
+                  </div>
                 </li>
-                <li>
-                  Valorisant les ressources culturelles et humaines locales pour
-                  un rayonnement international.
+                <li className=" flex items-center">
+                  <div className="mr-4">
+                    <CheckIcon className=" h-6 w-6 text-green-500" />
+                  </div>
+
+                  <div>
+                    Valorisant les ressources culturelles et humaines locales
+                    pour un rayonnement international.
+                  </div>
                 </li>
               </ul>
             </div>
             <div className="md:w-1/2" data-aos="fade-left">
               <img
-                src="/images/banniere/p6.png"
+                src="/images/banniere/p1.png"
                 alt="Développement durable"
                 className="h-64 w-full rounded-lg object-cover shadow-lg md:h-80"
                 width={500}
@@ -192,7 +264,7 @@ export default function WhyUs() {
       <div
         className="relative bg-cover bg-fixed bg-center px-6 py-20 text-center text-white"
         style={{
-          backgroundImage: "url(&apos;/images/banniere/p3.png&apos;)", // Remplacez par l&apos;URL de votre image
+          backgroundImage: "url('/images/banniere/p7.png')", // Remplacez par l&apos;URL de votre image
         }}
       >
         {/* Overlay */}
