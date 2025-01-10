e8f5a8384dcd4b42d644a851e99dcd6beba22e90
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-01-10 11:09:34 +0100

    v12.1

--- src/components/Master/MasterPrograms.tsx
+++ src/components/Master/MasterPrograms.tsx
@@ -39,7 +39,7 @@ const MasterPrograms: React.FC = () => {
   return (
     <div className="container mx-auto px-6 py-12">
       <h2 className="mb-10 text-center text-3xl font-bold text-gray-800 dark:text-white">
-        Nos Programmes de Maîtrise.
+        Nos Programmes de Master.
       </h2>
       <hr className="mb-20" />
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
