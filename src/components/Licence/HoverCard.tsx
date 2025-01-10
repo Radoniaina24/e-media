76b2976c6fa354e2c6290e62e769c394663a8866
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-01-10 13:11:11 +0100

    v12.9

--- src/components/Licence/HoverCard.tsx
+++ src/components/Licence/HoverCard.tsx
@@ -12,28 +12,30 @@ const HoverCard: React.FC<HoverCardProps> = ({
   link,
 }) => {
   return (
-    <div className="group relative w-full max-w-sm overflow-hidden rounded-lg shadow-lg">
-      {/* Image de fond */}
-      <div className="w-full">
-        <Image
-          src={imageSrc}
-          alt="Image de fond"
-          className="object-cover"
-          width={250}
-          height={250}
-        />
-      </div>
+    <Link href={link}>
+      <div className="group relative w-full max-w-sm overflow-hidden rounded-lg shadow-lg">
+        {/* Image de fond */}
+        <div className="w-full">
+          <Image
+            src={imageSrc}
+            alt="Image de fond"
+            className="object-cover"
+            width={250}
+            height={250}
+          />
+        </div>
 
-      {/* Overlay avec le bouton */}
-      <div className="absolute inset-0 flex items-end justify-center  bg-opacity-50 opacity-100  transition-opacity duration-300">
-        <Link
-          className="mb-8 rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white hover:bg-blue-700"
-          href={link}
-        >
-          {buttonText}
-        </Link>
+        {/* Overlay avec le bouton */}
+        <div className="absolute inset-0 flex items-end justify-center  bg-opacity-50 opacity-100  transition-opacity duration-300">
+          <Link
+            className="mb-8 rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white hover:bg-blue-700"
+            href={link}
+          >
+            {buttonText}
+          </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
