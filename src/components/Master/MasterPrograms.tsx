12a22e514c549481423d9b1af8583a2d66a2f1e9
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-01-12 17:29:35 +0300

    v13.2

--- src/components/Master/MasterPrograms.tsx
+++ src/components/Master/MasterPrograms.tsx
@@ -1,36 +1,40 @@
 "use client";
 import Link from "next/link";
-
+import Image from "next/image";
 const programs = [
   {
-    title: "MASTER EN INFORMATIQUE ET ÉLECTRONIQUE EMBARQUÉES (IEE)",
-    description:
-      "Ce programme forme des experts en développement de solutions technologiques innovantes pour les systèmes embarqués et les applications informatiques avancées.",
+    imageSrc:
+      "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736512786/univ-M-1_ikzy9d.png",
+    classe:
+      "absolute top-[258px] left-[60px]  flex items-end justify-center  bg-opacity-50 opacity-100 md:top-[167px] md:left-6 lg:top-[140px] lg:left-2 xl:top-[218px] xl:left-[45px] transition-opacity duration-300",
     link: "/formation/master/electronique_informatique",
   },
   {
-    title: "MASTER EN AUDIOVISUEL ET CINÉMATOGRAPHIE (AC)",
-    description:
-      "Un cursus dédié aux passionnés de production audiovisuelle et cinématographique, combinant techniques modernes et créativité.",
+    imageSrc:
+      "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736512786/univ-M-2_necsbs.png",
+    classe:
+      "absolute top-[205px] left-[115px]  flex items-end justify-center  bg-opacity-50 opacity-100 md:top-[130px] md:left-[62px] lg:top-[105px] lg:left-[45px] xl:top-[172px] xl:left-[92px] transition-opacity duration-300",
     link: "/formation/master/ac",
   },
   {
-    title: "MASTER EN MARKETING DIGITAL ET JOURNALISME (MDJ - M)",
-    description:
-      "Préparez-vous aux métiers de la communication digitale et du journalisme moderne en maîtrisant les outils et stratégies numériques.",
+    imageSrc:
+      "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736512785/univ-M5_cd5k4o.png",
+    classe:
+      "absolute top-[258px] left-[150px]  flex items-end justify-center  bg-opacity-50 opacity-100  md:top-[170px] md:left-[82px] lg:top-[140px] lg:left-[62px] xl:top-[220px] xl:left-[120px] transition-opacity duration-300",
     link: "/formation/master/mdj",
   },
   {
-    title:
-      "MBA SPÉCIALISÉ - MASTER OF BUSINESS ADMINISTRATION (FORMATION 100% EN LIGNE)",
-    description:
-      "Un programme en ligne axé sur le leadership et la gestion stratégique pour développer vos compétences en management.",
+    imageSrc:
+      "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736512785/univ-M-3_ov3yxn.png",
+    classe:
+      "absolute top-[288px] left-[280px]  flex items-end justify-center  bg-opacity-50 opacity-100  md:top-[188px] md:left-[172px] lg:top-[160px] lg:left-[138px] xl:top-[245px] xl:left-[230px] transition-opacity duration-300",
     link: "/formation/master/mba",
   },
   {
-    title: "MASTER EN DROIT (FORMATION 100% EN LIGNE)",
-    description:
-      "Une formation juridique approfondie, accessible à distance, pour devenir un expert en droit national et international.",
+    imageSrc:
+      "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736512785/univ-M-4_lnjdme.png",
+    classe:
+      "absolute top-[250px] left-[290px]  flex items-end justify-center  bg-opacity-50 opacity-100  md:top-[162px] md:left-[185px] lg:top-[132px] lg:left-[148px] xl:top-[212px] xl:left-[245px] transition-opacity duration-300",
     link: "/formation/master/droit",
   },
 ];
@@ -45,21 +49,11 @@ const MasterPrograms: React.FC = () => {
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
         {programs.map((program, index) => (
           <Link key={index} href={program.link}>
-            <div
-              className="flex h-full flex-col items-stretch justify-between rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105 hover:cursor-pointer hover:bg-gray-50 dark:bg-gray-dark dark:hover:bg-gray-700"
-              data-aos="fade-up"
-              data-aos-delay={index * 100} // Adding AOS animation delay for each card
-            >
-              <h3 className="mb-4 flex-grow text-center text-xl font-semibold text-gray-800 dark:text-gray-50">
-                {program.title}
-              </h3>
-              <p className="mb-6 flex-grow text-center text-gray-600 dark:text-gray-200">
-                {program.description}
-              </p>
-              <button className="mt-4 inline-block rounded bg-blue-500 px-6 py-3 text-sm text-white transition hover:bg-blue-600">
-                En savoir plus
-              </button>
-            </div>
+            <HoverCard
+              imageSrc={program.imageSrc}
+              link={program.link}
+              classe={program.classe}
+            />
           </Link>
         ))}
       </div>
@@ -67,3 +61,42 @@ const MasterPrograms: React.FC = () => {
   );
 };
 export default MasterPrograms;
+
+interface HoverCardProps {
+  imageSrc: string;
+  buttonText?: string;
+  link: string;
+  classe?: string;
+}
+
+const HoverCard: React.FC<HoverCardProps> = ({
+  imageSrc,
+  buttonText = "En savoir plus",
+  link,
+  classe = "absolute top-[170px] left-7  flex items-end justify-center  bg-opacity-50 opacity-100  transition-opacity duration-300",
+}) => {
+  return (
+    <div className="group relative w-full  overflow-hidden rounded-lg ">
+      {/* Image de fond */}
+      <div className="w-full">
+        <Image
+          src={imageSrc}
+          alt="Image de fond"
+          className="object-cover"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      {/* Overlay avec le bouton */}
+      <div className={classe}>
+        <Link
+          className=" rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white hover:bg-blue-700"
+          href={link}
+        >
+          {buttonText}
+        </Link>
+      </div>
+    </div>
+  );
+};
