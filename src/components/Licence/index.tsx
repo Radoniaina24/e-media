2f3e5e5b970d88f9750a077b36fd0fdda656d187
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-01-16 11:26:43 +0100

    v16.8

--- src/components/Licence/index.tsx
+++ src/components/Licence/index.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 import HoverCard from "./HoverCard";
 export default function Licence() {
-  const programs = [
+  const { language } = useLanguageContext();
+  const programsFr = [
     {
       link: "/formation/licence/tic", // Remplacez par le lien vers la page de la licence TIC
       imageSrc:
@@ -29,7 +30,34 @@ export default function Licence() {
         "https://res.cloudinary.com/dx3xhdaym/image/upload/v1736506071/drt_zzt0od.png", // Lien de l'image pour Droit
     },
   ];
-  const { language } = useLanguageContext();
+  const programsEn = [
+    {
+      link: "/formation/licence/tic", // Remplacez par le lien vers la page de la licence TIC
+      imageSrc:
+        "https://res.cloudinary.com/dx3xhdaym/image/upload/v1737022829/TIC_EN_q1tsql.jpg", // Lien de l'image pour TIC
+    },
+    {
+      link: "/formation/licence/can", // Remplacez par le lien vers la page de la licence CAN
+      imageSrc:
+        "https://res.cloudinary.com/dx3xhdaym/image/upload/v1737022830/CAN_EN_yup3w4.png", // Lien de l'image pour CAN
+    },
+    {
+      link: "/formation/licence/mdj", // Remplacez par le lien vers la page de la licence MDJ
+      imageSrc:
+        "https://res.cloudinary.com/dx3xhdaym/image/upload/v1737022829/MDJ_EN_n3ekfv.jpg", // Lien de l'image pour MDJ
+    },
+    {
+      link: "/formation/licence/management", // Remplacez par le lien vers la page de la licence Management
+      imageSrc:
+        "https://res.cloudinary.com/dx3xhdaym/image/upload/v1737022829/MGT_EN_rdsgsp.jpg", // Lien de l'image pour Management
+    },
+    {
+      link: "/formation/licence/droit", // Remplacez par le lien vers la page de la licence Droit
+      imageSrc:
+        "https://res.cloudinary.com/dx3xhdaym/image/upload/v1737022829/DRT_EN_duee53.jpg", // Lien de l'image pour Droit
+    },
+  ];
+  const programs = language === "fr" ? programsFr : programsEn;
   return (
     <div className="">
       <HeroSectionPrograms
@@ -45,7 +73,11 @@ export default function Licence() {
         <div className=" grid grid-cols-2 gap-5  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {programs.map((program, index) => (
             <Link href={program.link} key={index}>
-              <HoverCard link={program.link} imageSrc={program.imageSrc} />
+              <HoverCard
+                link={program.link}
+                imageSrc={program.imageSrc}
+                buttonText={language === "fr" ? "En savoir plus" : "Learn more"}
+              />
             </Link>
           ))}
         </div>
@@ -56,46 +88,6 @@ export default function Licence() {
   );
 }
 
-import Image from "next/image";
 import Link from "next/link";
 import { useLanguageContext } from "@/lib/context/LanguageContext";
 import HeroSectionPrograms from "../HeroSectionPrograms/HeroSectionPrograms";
-
-function StaircaseImages() {
-  return (
-    <div className="relative mx-auto w-full max-w-lg">
-      {/* Image 1 */}
-      <div className="absolute left-0 top-0">
-        <Image
-          src="https://res.cloudinary.com/dx3xhdaym/image/upload/v1736340927/son_v46pec.png"
-          alt="Image 1"
-          width={150}
-          height={150}
-          className="rounded-lg shadow-lg"
-        />
-      </div>
-
-      {/* Image 2 */}
-      <div className="absolute left-16 top-0">
-        <Image
-          src="https://res.cloudinary.com/dx3xhdaym/image/upload/v1736340927/son_v46pec.png"
-          alt="Image 2"
-          width={150}
-          height={150}
-          className="rounded-lg shadow-lg"
-        />
-      </div>
-
-      {/* Image 3 */}
-      <div className="absolute left-32 top-0">
-        <Image
-          src="https://res.cloudinary.com/dx3xhdaym/image/upload/v1736340927/son_v46pec.png"
-          alt="Image 3"
-          width={150}
-          height={150}
-          className="rounded-lg shadow-lg"
-        />
-      </div>
-    </div>
-  );
-}
