5b825307748991916a45775abcfb1e986cafa67a
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-04-14 14:40:40 +0300

    v30.3

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { fullname, email, subject, phone, message } = await req.json();
+    // Configurer le transporteur Nodemailer
+    const transporter = nodemailer.createTransport({
+      host: process.env.NEXT_PUBLIC_EMAIL_HOST,
+      port: parseInt(process.env.NEXT_PUBLIC_EMAIL_PORT || "587"),
+      secure: process.env.NEXT_PUBLIC_EMAIL_SECURE === "true",
+      auth: {
+        user: process.env.NEXT_PUBLIC_EMAIL_USER, // Ton email
+        pass: process.env.NEXT_PUBLIC_EMAIL_PASS, // Ton mot de passe ou App Password
+      },
+    });
+
+    // Contenu de l'email
+    const mailOptions = {
+      from: `"From Africa IT Summit"`,
+      to: "scolarite@e-mediauniversity.io",
+      cc: "dir.enligne@e-mediauniversity.io",
+      subject: `📩 Nouvelle contact - Site E-media Madagascar`,
+      html: `
+   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border-radius: 8px; background: #f4f4f4; color: #333;">
+      
+      <div style="background: #3396f3; padding: 20px; border-radius: 8px 8px 0 0; text-align: center;">
+        <h2 style="color: white; margin: 0;">📩 Nouveau message</h2>
+        <p style="color: #ddd; margin: 0;">Contact via E-media</p>
+      </div>
+
+      <div style="padding: 20px; background: white;">
+        <p><strong>👤 Full Name :</strong> ${fullname}</p>
+        <p><strong>📧 Email :</strong> <a href="mailto:${email}" style="color: #003366; text-decoration: none;">${email}</a></p>
+        <p><strong>💬 Message :</strong></p>
+        <blockquote style="border-left: 4px solid #003366; padding-left: 10px; margin: 10px 0; color: #555;">${message}</blockquote>
+      </div>
+
+      <div style="background: #3396f3; padding: 15px; border-radius: 0 0 8px 8px; text-align: center;">
+        <p style="color: white; margin: 0;">E-media Madagascar</p>
+      </div>
+
+    </div>
+  `,
+    };
+
+    // Envoyer l'email
+    await transporter.sendMail(mailOptions);
+    return NextResponse.json({
+      success: true,
+      message: "Email sent successfully!",
+    });
+  } catch (error) {
+    console.error("Erreur détaillée:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to send email." },
+      { status: 500 },
+    );
+  }
+}
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,8 +1,17 @@
 "use client";
 import { useLanguageContext } from "@/lib/context/LanguageContext";
+import { useFormik } from "formik";
 import React from "react";
-import HeroSectionPrograms from "../HeroSectionPrograms/HeroSectionPrograms";
-
+import * as yup from "yup";
+import Swal from "sweetalert2";
+import { Loader2 } from "lucide-react";
+const initialValues = {
+  fullname: "",
+  email: "",
+  subject: "",
+  phone: "",
+  message: "",
+};
 const Contact = () => {
   const { language } = useLanguageContext();
   const contact = language === "fr" ? "Contactez-nous" : "Contact Us";
@@ -10,13 +19,83 @@ const Contact = () => {
     language === "fr"
       ? "Si vous avez des questions ou des demandes, n'hésitez pas à nous contacter via ce formulaire."
       : "If you have any questions or requests, feel free to contact us through this form.";
+  const contactAfricaItSummitSchema = yup.object({
+    fullname: yup.string().required("This fullname is required"),
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("This email is required"),
+    message: yup.string().required("This message is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: contactAfricaItSummitSchema,
+    onSubmit: async (values, { setSubmitting, resetForm }) => {
+      setSubmitting(true); // Désactive le bouton
+      // console.log(values);
+      try {
+        const response = await fetch("/api/contact", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(values),
+        });
+        const result = await response.json();
+        if (result.success) {
+          Swal.fire({
+            title: "Great!",
+            text: "Your message has been sent successfully.",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 3000, // Disparition automatique après 3 sec
+            background: "#f4f4f4",
+            color: "#333",
+            customClass: {
+              popup: "rounded-xl shadow-lg",
+            },
+          });
+          // resetForm();
+        } else {
+          Swal.fire({
+            title: "Error!",
+            text: "Failed to send message.",
+            icon: "error",
+            showConfirmButton: false,
+            timer: 3000, // Disparition automatique après 3 sec
+            background: "#f4f4f4",
+            color: "#333",
+            customClass: {
+              popup: "rounded-xl shadow-lg",
+            },
+          });
+        }
+      } catch (error) {
+        Swal.fire({
+          title: "Error!",
+          text: "An unexpected error occurred.",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 3000, // Disparition automatique après 3 sec
+          background: "#f4f4f4",
+          color: "#333",
+          customClass: {
+            popup: "rounded-xl shadow-lg",
+          },
+        });
+      } finally {
+        setSubmitting(false); // Réactiver le bouton après réponse
+      }
+    },
+  });
+  const { values, handleChange, touched, errors, handleSubmit, resetForm } =
+    formik;
   return (
     <div id="contact" className="">
       {" "}
       <div className="container mx-auto px-4">
         <div className="-mx-4 flex flex-wrap">
           {/* Form Section */}
-          <div className="m-auto mb-12 w-full px-4 lg:mb-0 lg:w-7/12 xl:w-8/12">
+          <div className="m-auto mb-12 w-full px-4 lg:mb-0 lg:w-8/12 xl:w-8/12">
             <div className="rounded-lg bg-white p-8 shadow-lg sm:p-12 dark:bg-gray-800">
               <h2 className="mb-6 text-3xl font-semibold text-gray-800 dark:text-white">
                 {contact}
@@ -25,7 +104,7 @@ const Contact = () => {
                 {descritption}
               </p>
 
-              <form>
+              <form onSubmit={handleSubmit} autoComplete="off">
                 <div className="-mx-4 flex flex-wrap">
                   {/* Name Field */}
                   <div className="mb-6 w-full px-4 md:w-1/2">
@@ -33,18 +112,27 @@ const Contact = () => {
                       htmlFor="name"
                       className="mb-2 block text-sm font-medium text-gray-700 dark:text-white"
                     >
-                      {language === "fr" ? "Nom" : "Name"}
+                      {language === "fr" ? "Nom complet" : "Full Name"}
                     </label>
                     <input
                       type="text"
-                      id="name"
+                      id="fullname"
                       placeholder={
                         language === "fr"
-                          ? "Entrer votre nom"
-                          : "Enter your name"
+                          ? "Entrer votre nom complet"
+                          : "Enter your full name"
                       }
+                      value={formik.values.fullname}
+                      onChange={handleChange}
                       className="w-full rounded-md border border-gray-300 bg-gray-50 px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500"
                     />
+                    {errors.fullname && touched.fullname ? (
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className="font-medium"></span> {errors.fullname}
+                      </p>
+                    ) : (
+                      ""
+                    )}
                   </div>
 
                   {/* Email Field */}
@@ -63,8 +151,17 @@ const Contact = () => {
                           ? "Entrer votre email"
                           : "Enter your email"
                       }
+                      value={formik.values.email}
+                      onChange={handleChange}
                       className="w-full rounded-md border border-gray-300 bg-gray-50 px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500"
                     />
+                    {errors.email && touched.email ? (
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className="font-medium"></span> {errors.email}
+                      </p>
+                    ) : (
+                      ""
+                    )}
                   </div>
 
                   {/* Message Field */}
@@ -83,14 +180,33 @@ const Contact = () => {
                           ? "Entrer votre message"
                           : "Enter your message"
                       }
+                      value={formik.values.message}
+                      onChange={handleChange}
                       className="w-full rounded-md border border-gray-300 bg-gray-50 px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500"
                     ></textarea>
+                    {errors.message && touched.message ? (
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className="font-medium"></span> {errors.message}
+                      </p>
+                    ) : (
+                      ""
+                    )}
                   </div>
 
                   {/* Submit Button */}
                   <div className="w-full px-4">
-                    <button className="mt-4 w-full rounded-full bg-blue-600 py-3 font-medium text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-500 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-500">
-                      {language === "fr" ? "Envoyer" : "Send"}
+                    <button
+                      disabled={formik.isSubmitting}
+                      type="submit"
+                      className={`mt-4 w-full rounded-full py-3 font-medium text-white 
+    ${formik.isSubmitting ? "cursor-not-allowed bg-gray-400" : "bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-500 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-500"}`}
+                    >
+                      <div className="flex items-center justify-center gap-2">
+                        {/* Affiche le texte et le spinner quand le formulaire est en soumission */}
+
+                        {language === "fr" ? "Envoyer" : "Send"}
+                        {formik.isSubmitting ? <Loader2 /> : null}
+                      </div>
                     </button>
                   </div>
                 </div>
